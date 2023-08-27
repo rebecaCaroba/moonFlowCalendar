63b206acd73cb84c64e34ea4444b39c020f31283
rebecaCaroba <rebecacaroba@gmail.com>
2023-08-27 19:03:44 -0300

    Adicionando fonte

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { ThemeProvider } from "styled-components"
 import { defaultThemes } from "./styles/theme/default"
+import { GlobalStyle } from "./styles/global"
 
 export function App() {
  return (
   <ThemeProvider theme={defaultThemes}>
-
+    <GlobalStyle/>
   </ThemeProvider>
  )
 }
