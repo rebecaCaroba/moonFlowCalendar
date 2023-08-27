943873a5515ccae16c248b5bcac42a3d70e4b9be
rebecaCaroba <rebecacaroba@gmail.com>
2023-08-27 19:19:48 -0300

    Adicionando componente header

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,12 +1,16 @@
 import { Routes, Route } from "react-router-dom";
 import { Calendar } from "./pages/Calendar";
 import { Home } from "./pages/Home";
+import { DefaultLayout } from "./Layout/DefaultLayout";
 
 export function Router() {
     return (
         <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/calendar" element={<Calendar />} />
+        <Route path="/" element={<DefaultLayout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/calendar" element={<Calendar />} />
+        </Route>
       </Routes>
     )
 }
+
